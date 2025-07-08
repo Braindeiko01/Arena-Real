903523594bc5182bcef1173cdb6455dda10d2ccc
Braindeiko01 <124094024+Braindeiko01@users.noreply.github.com>
2025-07-08 12:12:35 -0500

    Import @Primary annotation (#208)

--- admin-back/src/main/java/com/example/admin/config/SecurityConfig.java
+++ admin-back/src/main/java/com/example/admin/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.admin.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
