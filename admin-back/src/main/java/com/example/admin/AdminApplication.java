8b8deea05dca7199b9e2f1b8c13b67c19cfa20b4
Braindeiko01 <124094024+Braindeiko01@users.noreply.github.com>
2025-07-07 23:47:36 -0500

    Configure admin service repository scanning

--- admin-back/src/main/java/com/example/admin/AdminApplication.java
+++ admin-back/src/main/java/com/example/admin/AdminApplication.java
@@ -2,8 +2,12 @@ package com.example.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.admin", "co.com.arena.real"})
+@EnableJpaRepositories("co.com.arena.real.infrastructure.repository")
+@EntityScan("co.com.arena.real.domain.entity")
 public class AdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
