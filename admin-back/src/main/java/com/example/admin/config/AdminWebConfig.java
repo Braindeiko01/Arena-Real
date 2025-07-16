f16ab9dc3da591269e02590f9f9f565df29fcb7d
Braindeiko01 <124094024+Braindeiko01@users.noreply.github.com>
2025-07-15 22:07:19 -0500

    Skip only repackage in back module

--- admin-back/src/main/java/com/example/admin/config/AdminWebConfig.java
+++ admin-back/src/main/java/com/example/admin/config/AdminWebConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class AdminWebConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
