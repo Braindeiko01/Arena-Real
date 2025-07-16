5f8f56cba749be88ca376ccb66cab3b6188187de
Braindeiko01 <124094024+Braindeiko01@users.noreply.github.com>
2025-07-15 22:35:06 -0500

    Restrict admin component scan

--- admin-back/src/main/java/com/example/admin/AdminApplication.java
+++ admin-back/src/main/java/com/example/admin/AdminApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"com.example.admin", "co.com.arena.real"})
+@SpringBootApplication(scanBasePackages = "com.example.admin")
 @EnableJpaRepositories("co.com.arena.real.infrastructure.repository")
 @EntityScan("co.com.arena.real.domain.entity")
 public class AdminApplication {
