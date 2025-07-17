f133f35812bb5dcfa75a9d3afc5cdc5aa217397d
Braindeiko01 <124094024+Braindeiko01@users.noreply.github.com>
2025-07-16 20:21:40 -0500

    Ensure Firebase auth state loads

--- front/src/store/ReduxProvider.tsx
+++ front/src/store/ReduxProvider.tsx
@@ -4,10 +4,14 @@ import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { store } from './index';
 import { refreshUserThunk } from './authSlice';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '@/lib/firebase';
 
 export const ReduxProvider = ({ children }: { children: React.ReactNode }) => {
   useEffect(() => {
+    const unsub = onAuthStateChanged(auth, () => {});
     store.dispatch(refreshUserThunk());
+    return () => unsub();
   }, []);
 
   return <Provider store={store}>{children}</Provider>;
