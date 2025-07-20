2bbc1974d704a9d248519404186185a65d1dd670
Braindeiko01 <124094024+Braindeiko01@users.noreply.github.com>
2025-07-19 20:46:58 -0500

    Add COOP/COEP headers

--- front/next.config.ts
+++ front/next.config.ts
@@ -17,6 +17,40 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    if (process.env.NODE_ENV !== 'production') {
+      return [
+        {
+          source: '/:path*',
+          headers: [
+            {
+              key: 'Cross-Origin-Opener-Policy',
+              value: 'same-origin-allow-popups',
+            },
+            {
+              key: 'Cross-Origin-Embedder-Policy',
+              value: 'unsafe-none',
+            },
+          ],
+        },
+      ]
+    }
+    return [
+      {
+        source: '/login',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
